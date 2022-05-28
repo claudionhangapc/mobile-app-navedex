935f93241579a849b1a8a9333a3e2fcf6def9088
claudionhangapc <claudionhangapc@gmail.com>
2022-05-28 14:22:53 -0300

    created flatlist home

--- src/screens/private/home/Home.js
+++ src/screens/private/home/Home.js
@@ -1,8 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import styles from './home.page.style'
 import CardNaver from '../../../components/private/CardNaver'
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  }
+];
 export default function Home() {
   return (
     <View style={styles.container}>
@@ -24,14 +43,17 @@ export default function Home() {
         </TouchableOpacity>
       
       </View>
-
-      <View style={{
+      
+      <FlatList
+       style={{
         flex:1,
-      }}>
-        <CardNaver/>
-        <CardNaver/>
-      </View>
-
+      }} 
+      keyExtractor={item => item.id}
+      renderItem ={({ item }) => (<CardNaver/>)}
+      data={DATA}
+      showsVerticalScrollIndicator={false}
+      />
+      
     </View>
   );
 }
